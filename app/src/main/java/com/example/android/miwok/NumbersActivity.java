2244ac5126f8f1fdf0b869986ce7fd92dccfbb2c
Desmend Jetton <djetton@iit.edu>
2018-07-06 00:09:02 -0500

    Included log messages for logcat files.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -22,6 +23,20 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+        //100 Days of Coding Day 1: Reset the streak due to 4th of July.
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+
+
+
 
     }
 }
