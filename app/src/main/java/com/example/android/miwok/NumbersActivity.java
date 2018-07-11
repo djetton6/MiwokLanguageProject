338d4a86223e67a15fe080239e37d709242c7692
Desmend Jetton <djetton@iit.edu>
2018-07-10 23:59:51 -0500

    Added list adapter for miwok NumbersActivity.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,24 +30,13 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        //Find the root view so we can add child views to it
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        // Keep looping until we've reached the end of the list (which means keep looping
-        // as long as the current index position is less than the length of the list.
-        // The index variable keeps track of our current position in the list and
-        // increments by 1 each time the code in the loop is executed.
-        for (int index = 0; index < words.size(); index++) {
-            TextView wordView = new TextView(this);
+        //Created an ArrayAdapter for adding vocabulary words to NumbersActivity.
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-            //Set the text to be word at the current index
-            wordView.setText(words.get(index));
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            // Add this TextView as another child to the root view of this layout
-            rootView.addView(wordView);
-
-
-        }
+        listView.setAdapter(itemsAdapter);
 
     }
 }
