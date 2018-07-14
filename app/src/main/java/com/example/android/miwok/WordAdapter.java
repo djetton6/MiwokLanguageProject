aa32b53379ac8c6493106ee30f3a66aed5d47c81
Desmend Jetton <djetton@iit.edu>
2018-07-14 01:37:54 -0500

    Updated the WordView Adapter with code from Android Version app. Changing data type views from 1 ImageView to 2 TextViews.

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -7,6 +8,13 @@ import android.widget.ArrayAdapter;
 public class WordAdapter extends ArrayAdapter<Word> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        //Check to see if existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+
+        }
+
         return super.getView(position, convertView, parent);
     }
 }
